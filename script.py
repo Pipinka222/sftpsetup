56cfdb333ed3d9e49d865a6ef3277fd3da8c7ca8
Pipinka222 <Nikitas0408@gmail.com>
2024-05-17 22:39:07 +0300

    Update script.py

--- script.py
+++ script.py
@@ -7,19 +7,17 @@ def install_packages():
     
     print("Installing OpenSSH server...")
     os.system('sudo apt-get install -y openssh-server')
-    
-    print("Installation complete.")
+    os.system('sudo systemctl start sshd')
 
 def configure_sftp():
     sshd_config_path = '/etc/ssh/sshd_config'
     
-    # Backup the original sshd_config file
-    os.system(f'sudo cp {sshd_config_path} {sshd_config_path}.backup')
-    
     with open(sshd_config_path, 'a') as sshd_config:
         sshd_config.write('\n')
+        sshd_config.write('PasswordAuthetication yes')
+        sshd_config.write('\n')
         sshd_config.write('Match Group sftpusers\n')
-        sshd_config.write('    ChrootDirectory %h\n')
+        sshd_config.write('    ChrootDirectory /srv/sftp\n')
         sshd_config.write('    ForceCommand internal-sftp\n')
         sshd_config.write('    AllowTcpForwarding no\n')
         sshd_config.write('    X11Forwarding no\n')
@@ -28,23 +26,24 @@ def configure_sftp():
 
 def create_sftp_user(username):
     print(f"Creating user {username}...")
-    os.system(f'sudo useradd -m -G sftpusers -s /bin/false {username}')
-    os.system(f'sudo passwd {username}')
-    
-    user_home_dir = f'/home/{username}'
-    os.system(f'sudo chown root:root {user_home_dir}')
-    os.system(f'sudo chmod 755 {user_home_dir}')
-    
-    sftp_dir = f'{user_home_dir}/uploads'
-    os.system(f'sudo mkdir -p {sftp_dir}')
-    os.system(f'sudo chown {username}:{username} {sftp_dir}')
-    
-    print(f"User {username} created and configured for SFTP.")
+    os.system('sudo mkdir /srv/sftp')
+    os.system('sudo groupadd sftpusers')
+    os.system('sudo useradd -G sftpusers -d /srv/sftp/{username} -s /sbin/nologin {username}')
+    os.system('sudo passwd {username}')
+    password=input("Enter the password for this user: ").strip()
+    os.system('password')
+    os.system('password')
+    print(f"User {username} created and configured for SFTP.") 
 
-def restart_sshd():
+def restart_sshd(username):
     print("Restarting SSH service...")
     os.system('sudo systemctl restart sshd')
     print("SSH service restarted.")
+def create_start_directory():
+    name=input("Enter the name of first directory: ")
+    os.system('sudo mkdir /srv/sftp/{name}')
+    os.system('sudo chown {username}:sftpusers /srv/sftp/{name}')
+    
 
 def main():
     install_packages()
@@ -54,6 +53,7 @@ def main():
     create_sftp_user(username)
     
     restart_sshd()
+    create_start_directory(username)
     print("SFTP server setup complete.")
 
 if __name__ == '__main__':
